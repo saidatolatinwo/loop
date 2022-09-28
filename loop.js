08e4d49b8f5b603c0f349d7df1c1e8aa9f52678b
saidatolatinwo <saidatolatinwo2020@gmail.com>
2022-09-28 13:35:45 +0100

    created the js file

---
+++ loop.js
@@ -0,0 +1,24 @@
+for (let i = 1; i <= 10; i++) {
+    console.log (i)
+}
+
+let num = 0;
+while (num < 10) {
+    console.log(num);
+    num++;
+}
+
+console.log(`odd number`)
+for ( let i=1; i<=100; i+=2){
+    console.log(i);
+}
+
+console.log(`multiple of 5`)
+for (let j=5; j<=100; j+=5){
+    console.log(j);
+}
+
+console.log(`prime number`)
+for (let k=1; k<=100; k+=3){
+    console.log(k)
+}
